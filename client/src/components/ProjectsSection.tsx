ab2ef3f25bc3d4c0e40dad582d3357295883651f
Vinay Datta <vinaydattarao@gmail.com>
2026-03-25 12:12:17 +0530

    Update ProjectsSection.tsx

--- client/src/components/ProjectsSection.tsx
+++ client/src/components/ProjectsSection.tsx
@@ -14,7 +14,7 @@ const projects = [
     id: 2,
     title: "Employee Management System",
     description: "Designed a GUI to handle employee records, attendance, salaries, and departmental data.",
-    image: "https://images.pexels.com/photos/33797203/pexels-photo-33797203.jpeg",
+    image: "https://images.pexels.com/photos/36731104/pexels-photo-36731104.jpeg",
     technologies: ["Python", "SQLite", "Tkinter"],
     exploreUrl: "https://github.com/VinayDattarao/Employee-Management-System"
   },
